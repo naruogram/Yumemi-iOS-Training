c0d5c977d0d76c6ec3a537be97731323dedfc668
Yoshiki Naruo <yoshiki20010524@icloud.com>
2022-06-16 10:52:14 +0900

    Initial Commit

---
+++ Yumemi-iOS-Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yumemi-iOS-Training
+//
+//  Created by 成尾 嘉貴 on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
